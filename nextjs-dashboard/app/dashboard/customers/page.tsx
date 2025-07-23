37ad371a9bc9a53e9aa076c7afb4b861726e3e31
Allan Brockenbrough <31295179+brockenbrough@users.noreply.github.com>
2025-07-23 14:06:23 -0400

    Turns back on dynamic generation for pages.

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,5 +1,3 @@
-// TODO: i've turned off generating static pages due to vercel error connecting to the database during building.
-//export const dynamic = 'force-dynamic';
 
 export default function Page() {
     return <p>Customers Page</p>;
