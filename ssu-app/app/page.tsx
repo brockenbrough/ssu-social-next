c200fd2b9153c81280388bf602295110f8efd8f2
Allan Brockenbrough <31295179+brockenbrough@users.noreply.github.com>
2025-07-28 11:05:56 -0400

    A trivial home page change.

--- ssu-app/app/page.tsx
+++ ssu-app/app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Professor Brockenbrough&apos;s Next Skeleton.</strong> This is a modified version of 
+            <strong>Welcome to Professor Brockenbrough&apos;s Next skeleton.</strong> This is a modified version of 
             the one described in the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
