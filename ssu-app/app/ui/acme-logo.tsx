41178a63f20409e41c4757d340dc13ce10a7ff4f
Lindsey McGovern <lindsey.mcgovern.s25@nala.pet>
2025-09-05 10:35:27 -0400

    Lindsey's change #2!!!!

--- ssu-app/app/ui/acme-logo.tsx
+++ ssu-app/app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${comicNeue.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">LINDSEY IS THE BOMB.COM😎</p>
+      <p className="text-[44px]">Salem State Skeleton😎</p>
     </div>
   );
 }
