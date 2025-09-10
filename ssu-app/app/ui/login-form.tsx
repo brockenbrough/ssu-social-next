b6e30358f105555b296e680f7e59af6500b2d41b
Merry567 <meredithwonson@gmail.com>
2025-09-09 21:50:59 -0400

    change to email input box text

--- ssu-app/app/ui/login-form.tsx
+++ ssu-app/app/ui/login-form.tsx
@@ -40,7 +40,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="give me your email address, I probably wont fill your inbox with spam..."
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
