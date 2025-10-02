2d599d53241f2cbe03e7be6556f01abec5f5ee58
Salaiev <96998535+Salaiev@users.noreply.github.com>
2025-10-02 09:29:02 -0400

    adding files

--- ssu-app/app/lib/data.ts
+++ ssu-app/app/lib/data.ts
@@ -11,7 +11,7 @@ import {
 import { formatCurrency } from './utils';
 
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 // Notice that this file contains utility functions that are called internally by the server components.
 // Example: the user requests a page that contains a server component <RevenueChart> that calls fetchRevenue().
