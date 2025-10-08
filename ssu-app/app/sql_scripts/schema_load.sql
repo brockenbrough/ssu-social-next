1bb907d11a04a540312b3a09dac162db6cf9a33c
victor <victordmazo@gmail.com>
2025-10-08 10:23:12 -0400

    added hashtag and like parameters back in

--- ssu-app/app/sql_scripts/schema_load.sql
+++ ssu-app/app/sql_scripts/schema_load.sql
@@ -35,6 +35,30 @@ BEGIN
         );
     END IF;
 
+     IF NOT EXISTS (SELECT 1 FROM hashtags WHERE hashtag = '#TestTag') THEN
+        INSERT INTO hashtags (
+            hashtag
+        )
+        VALUES (
+            '#TestTag'
+        );
+    END IF;
+
+ IF NOT EXISTS (
+        SELECT 1 FROM likes WHERE user_id = fixed_user_id1 AND post_id = fixed_post_id
+    ) THEN
+        INSERT INTO likes (
+            post_id,
+            user_id,
+            created_at
+        )
+        VALUES (
+            fixed_post_id,
+            fixed_user_id2,
+            NOW()
+        );
+    END IF;
+    
     IF NOT EXISTS (SELECT 1 FROM ssu_users WHERE user_id = fixed_user_id2) THEN
         INSERT INTO ssu_users (
             user_id,
