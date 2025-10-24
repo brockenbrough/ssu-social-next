332a46b9252c92d1d598ffe5e37780f77dc35468
mx-vin <spraggins0602@gmail.com>
2025-10-24 09:48:13 -0400

    cors for all post routes angel did

--- ssu-app/app/api/posts/GetPostPage/route.ts
+++ ssu-app/app/api/posts/GetPostPage/route.ts
@@ -1,6 +1,7 @@
 // app/api/posts/getpostpage/route.ts
 import { NextResponse } from "next/server";
 import postgres from "postgres";
+import { corsHeaders } from "@/utilities/cors";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
--- ssu-app/app/api/posts/getPostById/[id]/route.ts
+++ ssu-app/app/api/posts/getPostById/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import postgres from "postgres";
+import { corsHeaders } from "@/utilities/cors";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
